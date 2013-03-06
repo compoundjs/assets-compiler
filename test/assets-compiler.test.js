0959d15deefcb05537c29925eefa0661dcbdb699
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-07 00:17:55 +0700

    Init compound

---
+++ test/assets-compiler.test.js
@@ -0,0 +1,9 @@
+describe('assets-compiler', function() {
+    var app = getApp();
+    app.enable('merge stylesheets');
+    var compound = app.compound;
+
+    it('should work', function() {
+
+    });
+});
