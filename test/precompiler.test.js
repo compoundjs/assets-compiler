db71bc71d8a1570776fadfe00462cab8f16cd1ef
Sascha Gehlich <sascha@gehlich.us>
2013-03-16 21:32:22 +0100

    stylesheet compiler tests, coffeescript compiler tests, moved helper functions to utils.js

--- test/precompiler.test.js
+++ test/precompiler.test.js
@@ -1,12 +1,13 @@
 var fs = require('fs')
-  , path = require('path');
+  , path = require('path')
+  , utils = require('./utils');
 
 describe('assets-compiler', function() {
 
   describe('precompilation', function() {
 
     it('should precompile stylesheets', function (done) {
-      invalidateRequireCache(); // To get a fresh compound app
+      utils.invalidateRequireCache(); // To get a fresh compound app
 
       var app = getApp();
       var compound = app.compound;
@@ -17,7 +18,7 @@ describe('assets-compiler', function() {
 
       // Make sure previously precompiled assets are deleted
       var cssDir = app.root + '/public/stylesheets';
-      ensureDirClean(cssDir);
+      utils.ensureDirClean(cssDir);
 
       // Wait some time for precompiler to finish
       // TODO: Replace this with an event
@@ -26,15 +27,14 @@ describe('assets-compiler', function() {
         fs.existsSync(path.resolve(cssDir, 'stuff.css')).should.be.true;
 
         // Clean up
-        ensureDirClean(cssDir, function() {
-          done();
-        });
+        utils.ensureDirClean(cssDir);
+        done();
       }, 500);
 
     });
 
     it('should precompile coffeescripts', function (done) {
-      invalidateRequireCache(); // To get a fresh compound app
+      utils.invalidateRequireCache(); // To get a fresh compound app
 
       var app = getApp();
       var compound = app.compound;
@@ -45,7 +45,7 @@ describe('assets-compiler', function() {
 
       // Make sure previously precompiled assets are deleted
       var jsDir = app.root + '/public/javascripts';
-      ensureDirClean(jsDir);
+      utils.ensureDirClean(jsDir);
 
       // Wait some time for precompiler to finish
       // TODO: Replace this with an event
@@ -53,9 +53,8 @@ describe('assets-compiler', function() {
         fs.existsSync(path.resolve(jsDir, 'application.js')).should.be.true;
 
         // Clean up
-        ensureDirClean(jsDir, function() {
-          done();
-        });
+        utils.ensureDirClean(jsDir);
+        done();
       }, 500);
 
     });
@@ -63,30 +62,3 @@ describe('assets-compiler', function() {
   });
 
 });
-
-function ensureDirClean(dir, callback) {
-    if(!callback) callback = function(){};
-    fs.exists(dir, function(exists) {
-        if (exists) {
-            fs.readdir(dir, function(err, files) {
-                files.filter(function (file) {
-                    return file.substr(0, 1) !== '.'
-                }).map(function(file) {
-                    return path.join(dir, file);
-                }).forEach(fs.unlinkSync);
-
-                callback();
-            });
-        } else {
-            fs.mkdir(dir, 0755);
-            callback();
-        }
-    });
-}
-
-
-function invalidateRequireCache() {
-  Object.keys(require.cache).forEach(function (key) {
-    delete require.cache[key];
-  });
-};
